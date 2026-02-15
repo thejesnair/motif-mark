bfe6a81e41a88dda02b9e16bee81163dc98479e7
Thejes Nair <tnair@uoregon.edu>
2026-02-15 15:24:58 -0800

    oneline fasta, find regions, SplicingRegion class

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -6,9 +6,6 @@ Takes in FASTA file and generates image marking motifs along gene
 Script outputs both an svg and png
 '''
 
-from bioinfo import oneline_fasta # converts fasta record to oneline
-from bioinfo import validate_base_seq # determine if RNA (true) or DNA seq
-
 import argparse
 import re # regex
 
@@ -16,21 +13,21 @@ def get_args():
     '''Takes in command line arguments for paths to motifs files, FASTA file, and output image.'''
     parser = argparse.ArgumentParser(description=
                                      "Program to generate an image of motifs along a gene")
-    #parser.add_argument("-f", "--file", help="path for fasta file", type=str, required=True)
+    parser.add_argument("-f", "--file", help="path for fasta file", type=str, required=True)
     parser.add_argument("-m", "--motif", help="deduped SAM file, absolute path", type=str, required=True)
     #parser.add_argument("-o", "--outfile", help = "name for outputted motif image (png)", type=str, required=True)
 
     return parser.parse_args()
 
 args = get_args()
-#fasta = args.file
+input_fasta = args.file
 motif_file = args.motif
 #image = args.outfile
 
 ## HELPER FXN ##
 
 # read in fasta
-def read_fasta(input_fa:str) -> str:
+def read_fasta(input_fa:str) -> list[tuple[str,str]]:
     ''' Parses a FASTA file and returns a list of (header, sequence) tuples
         Handles multiline sequences and stores seq as oneline '''
     
@@ -39,7 +36,7 @@ def read_fasta(input_fa:str) -> str:
     records = []
     
     with open(input_fa, 'r') as in_fa:
-        for i, line in enumerate(in_fa):
+        for line in in_fa:
             line = line.strip()
             if line.startswith('>'):    # new header, new record starting
                 if current_header is not None:      # if there is already have a record in store, save
@@ -56,6 +53,7 @@ def read_fasta(input_fa:str) -> str:
             records.append((current_header, ''.join(current_seq)))
     return records
 
+records = read_fasta(input_fasta)
 
 # motifs list
 with open(motif_file) as f:
@@ -64,8 +62,7 @@ with open(motif_file) as f:
 print(motifs) #check that properly pulls motifs
 
 ## CLASSES ##
-
-# create Motif objects 
+ 
 class Motif:
     ''' Holds one motif definition, regex pattern, ambiguity rules 
         What is the motif I'm looking for? '''
@@ -90,7 +87,6 @@ class Motif:
     
     def __init__(self, motif, mode):
 
-        ## DATA
         self.motif = motif
         self.mode = mode
 
@@ -115,6 +111,7 @@ class Motif:
 
     ## METHODS
     def build_regex_body(self) -> str:
+        ''' Build regex expression for motif '''
         regex_exp = []
         for char in self.pattern.upper():       # upper bc of dict
             if char in self.degenerate_map:
@@ -124,16 +121,62 @@ class Motif:
         return ''.join(regex_exp)
 
     def build_lookahead_overlap_regex(self) -> str:
+        ''' Build regex expression for lookahead/overlap '''
         return "(?=(" + self.regex_body + "))"      # (?=...) is a lookahead assertion!
 
 class SplicingRegion:
     ''' Represents FASTA record
         Where I'm looking for motifs '''
     
-    def __init__(self, header, sequence):
+    def __init__(self, header: str, sequence: str):
         self.header = header
-        self.sequence = self.oneline_fasta()       # original sequence
+        self.sequence = sequence       # original sequence
         self.sequence_upper = sequence.upper()    # to identify motifs
+
+        self.introns = []
+        self.exons = []
+
+        self.mode = self.detect_mode()
+        self.find_intron_exon_regions()
+
+    ## METHODS
+    def detect_mode(self) -> str:
+        ''' Determine if DNA or RNA '''
+        if "U" in self.sequence_upper:
+            return "RNA"
+        elif "T" in self.sequence_upper:
+            return "DNA"
+        else:
+            raise ValueError("DNA or RNA FASTA required")
+        
+
+    def find_intron_exon_regions(self):
+        ''' Determines coordinates of introns and exons
+            Tuple: Intron/exon number, start pos, end pos '''
+
+        intron_num = 0
+        exon_num = 0
+        seq = self.sequence
+        n = len(seq)
+        i = 0       # start coordinate
+        j = 0       # 'scanning' end coordinate
+
+        while i < n:
+            if seq[i].islower():
+                j = i       # start j at current i location
+                while j < n and seq[j].islower():
+                    j += 1
+                intron_num += 1
+                self.introns.append((intron_num, i, j))
+                i = j       # move i to j for new location of next region
+            elif seq[i].isupper():
+                j = i
+                while j < n and seq[j].isupper():
+                    j += 1
+                exon_num += 1
+                self.exons.append((exon_num, i, j))
+                i = j
+        return (self.introns, self.exons)
 
 
 class MotifLocation:
